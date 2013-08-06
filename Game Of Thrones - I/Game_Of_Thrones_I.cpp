7770bc44db406db4717bc228f0b794eb7029b5f4
Sephiroth-XIII <safrath_13@hotmail.com>
2013-08-06 14:11:37 +0530

    Challenge: Game of Thrones-I

---
+++ Game Of Thrones - I/Game_Of_Thrones_I.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() 
+{
+    char in;
+    bitset<26> A;
+    while(cin>>in)
+    {
+        A.flip(in-'a');
+    }
+    if(A.count()<=1)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    return 0;
+}
